ed5879b6ca2a1deb57e96135ffa430e2d862bd79
guoyi0116 <trundle410821@gmail.com>
2022-03-16 16:56:34 +0800

    加入專案檔案。

---
+++ 20220316-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220316_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R=9999;
+            double num1, num2;
+            string op;
+            Console.WriteLine("請輸入第一個數");
+            num1=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("請輸入第二個數");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("請輸入運算子");
+            op = Console.ReadLine();
+            switch (op)
+            {
+                case"+":
+                    Console.WriteLine("{0}",num1+num2);
+                    break;
+                case "-":
+                    Console.WriteLine("{0}", num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine("{0}", num1 *num2);
+                    break;
+                case "/":
+                    Console.WriteLine("{0}", num1 / num2);
+                    break;
+                default:
+                    Console.WriteLine("無效運算");
+                    break;
+
+            }
+            Console.WriteLine("計算算結果為{0}",R);
+            Console.Read();
+        }
+    }
+}
